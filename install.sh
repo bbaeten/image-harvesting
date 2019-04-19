924fad71f56f234a8d8a65182234b94988115c53
Ben Baeten <bbaeten@fulfillnetinc.com>
2019-04-19 09:28:48 -0500

    added install file

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python -m pip install vlc
+python -m pip install python-vlc
+python -m pip install beautifulsoup4
+python -m pip install pafy
+python -m pip install urllib3
+python -m pip install requests
